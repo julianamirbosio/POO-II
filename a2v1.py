0c967f385efe4f4071f39f1994d43306eed76af5
Juliana <128743881+julianamirbosio@users.noreply.github.com>
2024-03-27 00:06:18 -0300

    Update a2v2.py

--- a2v1.py
+++ a2v1.py
@@ -1,27 +1,47 @@
-'''2.1. Crie uma pilha padr˜ao usando orienta¸c˜ao a objetos usando um tipo
-qualquer de sua preferˆencia.'''
- 
-class Pilha():
-    def __init__(self, cpilha):
-        self.cpilha = cpilha
-    
-    def empilhar(self, cpilha):
-        pass
-    
-    def desempilhar():
-        pass
-    
-mensagem = input("Bem vindo, deseja criar uma pilha: ")
+'''2.1. Crie uma pilha padrão usando orientação a objetos usando um tipo qualquer de sua preferência.'''
 
-if mensagem.lower() == "sim":
-    print("Para encerrar o empilhamento, insira asterisco (*) ")
+class Pilha:
+    def __init__(self, cpilha):
+        self.cpilha= cpilha 
+        tamanho = len(cpilha)
+
+    def empilhar(self, cobjeto): #push
+        self.cpilha = self.cpilha.append(cobjeto)
+
+    def desempilhar(self, cobjeto): #pop
+        for i
+
+print("Insira, linha a linha, os itens da pilha (os primeiros estaram no fundo da pilha e os ultimos no topo): ")
+print("Para encerrar o empilhamento, insira asterisco (*) ")
+print()
+
+pilha = [] #Definindo uma pilha inicial
+while True:
+    objeto = input()
+    if objeto == '*':
+        print("Programa encerrado")
+        break
+    else:
+        pilha.append(objeto)
+ppilha = Pilha(pilha)
+
+while True:
+  print("A qualquer momento, para consultar a pilha, digite 'pilha'. Para empilhar, digite 'empilhar'. Para desempilhar, digite 'desempilhar'. Para encerrar o programa, digite 'sair'")
+  print()
+  comando = input()
+  
+  if comando == 'pilha':
+      print(ppilha)
     
-    pilha = []
-    while True:
-        objeto = input()
-        if objeto == '*':
-            print("Programa encerrado")
-            break
+  elif comando == 'empilhar':
+      objeto = input("Insira o objeto que deseja empilhar: ")
+      ppilha.empilhar(objeto)
+          
+  elif comando == 'desempilhar':
+    
+  elif comando == 'sair':
+      print("Programa encerrado")
+      break
         else:
             pilha.append(objeto)
 
